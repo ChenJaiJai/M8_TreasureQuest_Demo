258b4257a6109751d342301a286c1b2e62b1041a
JaiJai <wm.rd01@gmail.com>
2025-05-14 09:29:30 +0800

    大更新

--- application.762e2.js
+++ application.762e2.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.0de8d.json';
+          this.settingsPath = 'src/settings.dde9f.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.7546d.js
+++ index.7546d.js
@@ -1,4 +1,4 @@
-System.register(["./application.9f547.js"], function (_export, _context) {
+System.register(["./application.762e2.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
